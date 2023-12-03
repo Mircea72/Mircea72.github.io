7287563c34b02ff3f9edafc9b671b57fba473a94
Mircea72 <mircris36@yahoo.com>
2023-12-03 03:52:37 +0200

    Cod revizuit 2 Dec.

---
+++ index.js
@@ -0,0 +1,35 @@
+function showHome() {
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+
+  var page = document.getElementById("home");
+  page.style.display = "block";
+}
+
+function showSkills() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+
+  var page = document.getElementById("skills");
+  page.style.display = "block";
+}
+
+function showProjets() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+
+  var page = document.getElementById("projects");
+  page.style.display = "block";
+}
+
+function showLanguages() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+
+  var page = document.getElementById("languages");
+  page.style.display = "block";
+}
